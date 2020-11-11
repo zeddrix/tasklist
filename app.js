6b819731caf51039a09ec0649c13c2ab06a7e198
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-11 15:18:38 +0800

    Able to persist to localStorage when task added.

--- app.js
+++ app.js
@@ -4,19 +4,39 @@ const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
 
+let li;
+
+const createTaskElements = () => {
+  li = document.createElement("li");
+  li.className = "collection-item";
+  const link = document.createElement("a");
+  link.className = "delete-item secondary-content";
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  li.appendChild(link);
+};
+
+const storeTaskInLS = (task) => {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
 const addTask = (e) => {
   if (taskInput.value === "") {
     alert("Add a task");
   } else {
-    const li = document.createElement("li");
-    li.className = "collection-item";
+    createTaskElements();
     li.appendChild(document.createTextNode(taskInput.value));
-    const link = document.createElement("a");
-    link.className = "delete-item secondary-content";
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    li.appendChild(link);
 
     taskList.appendChild(li);
+    storeTaskInLS(taskInput.value);
 
     taskInput.value = "";
     e.preventDefault();
@@ -28,7 +48,6 @@ const removeTask = (e) => {
     if (confirm("Are you sure?")) {
       e.target.parentElement.parentElement.remove();
     }
-  } else {
   }
 };
 
@@ -56,6 +75,23 @@ const loadEventListeners = () => {
   taskList.addEventListener("click", removeTask);
   clearBtn.addEventListener("click", clearTasks);
   filter.addEventListener("keyup", filterTasks);
+  document.addEventListener("DOMContentLoaded", getTasksFromLS);
+};
+
+const getTasksFromLS = () => {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach((task) => {
+    createTaskElements();
+    li.appendChild(document.createTextNode(task));
+
+    taskList.appendChild(li);
+  });
 };
 
 loadEventListeners();
