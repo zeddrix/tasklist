8cad95a407d24944774752e1b0090c7ce0c1f76b
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-11 14:29:56 +0800

    Just formatted.

--- app.js
+++ app.js
@@ -8,20 +8,19 @@ const addTask = (e) => {
   if (taskInput.value === "") {
     alert("Add a task");
   } else {
-     const li = document.createElement('li');
-     li.className = 'collection-item';
-     li.appendChild(document.createTextNode(taskInput.value));
-     const link = document.createElement('a');
-     link.className = 'delete-item secondary-content';
-     link.innerHTML = '<i class="fa fa-remove"></i>';
-     li.appendChild(link);
+    const li = document.createElement("li");
+    li.className = "collection-item";
+    li.appendChild(document.createTextNode(taskInput.value));
+    const link = document.createElement("a");
+    link.className = "delete-item secondary-content";
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
 
-     taskList.appendChild(li);
+    taskList.appendChild(li);
 
-     taskInput.value = '';
-     e.preventDefault();
+    taskInput.value = "";
+    e.preventDefault();
   }
-
 };
 
 const loadEventListeners = () => {
